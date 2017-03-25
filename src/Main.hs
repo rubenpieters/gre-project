9dc869fd852b767662ac5a8d04cc1db1e0f8ec7b
rubenpieters <ruben.pieters2@pandora.be>
2017-03-25 18:53:45 +0100

    most basic working engine, using lenses

--- src/Main.hs
+++ src/Main.hs
@@ -1,31 +1,85 @@
-module Main where
+{-# LANGUAGE TemplateHaskell #-}
+
+--module Main where
 
 import Control.Lens
 
 data Card =
   NumberCard Int
   | BuffCard Location Int
+  deriving (Show, Eq)
 
-data Location = Board | Hand | Deck -- | Mult Location Location
+buff f (NumberCard x) = NumberCard (f x)
+buff _ c@BuffCard{} = c
+
+data Location = Board | Hand | Deck deriving (Show, Eq)
 
 type Board = [Card]
 type Hand = [Card]
 type Deck = [Card]
 
 data PlayerState = PlayerState
-  { hand :: Hand
-  , deck :: Deck
-  , board :: Board
+  { _hand :: Hand
+  , _deck :: Deck
+  , _board :: Board
   }
+  deriving (Show, Eq)
 
-type GameState = [PlayerState]
+makeLenses ''PlayerState
+
+locAsLens Board = board
+locAsLens Hand = hand
+locAsLens Deck = hand
+
+data GameState = GameState
+  { _playerState :: [PlayerState]
+  , _playerTurn :: Int
+  }
+  deriving (Show)
+
+makeLenses ''GameState
 
 type PlayerTurn = Int
+type TurnBound = Int
 
 playCard :: PlayerTurn -> Card -> GameState -> GameState
-playCard pt c@(NumberCard nc) gameState = set (element pt) newPlayerState gameState where
-  playerState = gameState !! pt
-  newPlayerState = set (element 0) c (board playerState)
+playCard pt c@NumberCard{} gameState =
+  gameState & (playerState . element pt . board . element 0) .~ c
+playCard pt (BuffCard loc amount) gameState =
+  gameState & (playerState . element pt . locAsLens loc . traverse) %~ buff (+ amount)
+
+drawCard :: PlayerState -> PlayerState
+drawCard ps@PlayerState {_deck = [], _hand = _} = ps
+-- we can prepend this if the order within hand doesn't matter
+-- currently playing cards is based on the first element, so it does
+drawCard ps@PlayerState {_deck = (c:d), _hand = h} = ps & deck .~ d & hand .~ h ++ [c]
+
+discardCard :: PlayerState -> PlayerState
+discardCard ps@PlayerState {_hand = []} = ps
+discardCard ps@PlayerState {_hand = (_:h)} = ps & hand .~ h
+
+executeTurn :: PlayerTurn -> GameState -> GameState
+executeTurn pt gameState = afterDiscardCard where
+  afterDrawCard = gameState & (playerState . element pt) %~ drawCard
+  maybeCardToPlay :: Maybe Card
+  maybeCardToPlay = afterDrawCard ^? (playerState . element pt . hand . element 0)
+  play :: Maybe Card -> GameState -> GameState
+  play (Just c) = playCard pt c
+  play Nothing = id
+  afterPlayCard = play maybeCardToPlay afterDrawCard
+  afterDiscardCard = afterPlayCard & (playerState . element pt) %~ discardCard
+
+advancePlayerTurn :: TurnBound -> PlayerTurn -> PlayerTurn
+advancePlayerTurn bound current =
+  if candidate > bound
+  then 0
+  else candidate
+  where
+  candidate = current + 1
+
+advanceGame :: TurnBound -> GameState -> GameState
+advanceGame bound gameState = nextGameState & playerTurn %~ advancePlayerTurn bound where
+  nextGameState = executeTurn (_playerTurn gameState) gameState
 
 main :: IO ()
 main = do
