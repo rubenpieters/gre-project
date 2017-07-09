3045bd649fd009d4ae84be506e1633fba987546d
rubenpieters <ruben.pieters2@pandora.be>
2017-07-09 14:56:06 +0200

    fix tests

--- shared/test/LibSpec.hs
+++ shared/test/LibSpec.hs
@@ -19,6 +19,9 @@ instance Arbitrary CardModType where
 instance Arbitrary CardCappingType where
   arbitrary = genericArbitrary' Z uniform
 
+instance Arbitrary RangeType where
+  arbitrary = genericArbitrary' Z uniform
+
 instance Arbitrary CardProps where
   arbitrary = genericArbitrary' Z uniform
 
@@ -67,64 +70,65 @@ spec = do
       \ps -> length (discardCard ps ^. hand) <= length (ps ^. hand)
   describe "card1" $ do
     it "should work in basic case" $ do
-      let newBoard = playCard 0 card1 testGS_Board123
+      newBoard <- playCard 0 card1 testGS_Board123
       newBoard `shouldBe` (testGS_Board123 & (playerState . element 0 . board . element 0) .~ NumberCard 10)
   describe "card2" $ do
     it "should work in basic case" $ do
-      let newBoard = playCard 0 card2 testGS_Board123
+      newBoard <- playCard 0 card2 testGS_Board123
       newBoard `shouldBe` (testGS_Board123 & (playerState . element 0 . board . element 0) .~ NumberCard 6
                                            & (playerState . element 1 . board . element 0) .~ NumberCard 6
                           )
   describe "card3" $ do
     it "should work in basic case" $ do
-      let newBoard = playCard 0 card3 testGS_All0
+      newBoard <- playCard 0 card3 testGS_All0
       newBoard `shouldBe` (testGS_All0 & (playerState . element 0 . board) .~ replicate 3 (NumberCard 2))
   describe "card4" $ do
     it "should work in basic case" $ do
       let gs = testGS_B1B2 (map NumberCard [0, 1, 0]) (map NumberCard [0, 1, 0])
-      let newBoard = playCard 0 card4 gs
+      newBoard <- playCard 0 card4 gs
       newBoard `shouldBe` (gs & (playerState . element 0 . board) .~ replicate 3 (NumberCard 1))
     it "should work in highest is rightmost case" $ do
       let gs = testGS_B1B2 (map NumberCard [0, 0, 1]) (map NumberCard [0, 1, 0])
-      let newBoard = playCard 0 card4 gs
+      newBoard <- playCard 0 card4 gs
       newBoard `shouldBe` (gs & (playerState . element 0 . board) .~ map NumberCard [0, 1, 1])
     it "should work in highest is leftmost case" $ do
       let gs = testGS_B1B2 (map NumberCard [1, 0, 0]) (map NumberCard [0, 1, 0])
-      let newBoard = playCard 0 card4 gs
+      newBoard <- playCard 0 card4 gs
       newBoard `shouldBe` (gs & (playerState . element 0 . board) .~ map NumberCard [1, 1, 0])
   describe "card5" $ do
     it "should work in basic case" $ do
-      let newBoard = playCard 0 card5 testGS_Board123
+      newBoard <- playCard 0 card5 testGS_Board123
       newBoard `shouldBe` (testGS_Board123 & (playerState . element 0 . board) .~ map NumberCard [0, 2, 4])
   describe "card6" $ do
     it "should work in basic case " $ do
-      let newBoard = playCard 0 card6 testGS_Board123
+      newBoard <- playCard 0 card6 testGS_Board123
       newBoard `shouldBe` (testGS_Board123 & (playerState . element 0 . board) .~ map NumberCard [2, 2, 2]
                                            & (playerState . element 1 . board) .~ map NumberCard [2, 2, 2]
                           )
   describe "card7" $ do
     it "should work in basic case" $ do
-      let newBoard = playCard 0 card7 testGS_HandDeck0
+      newBoard <- playCard 0 card7 testGS_HandDeck0
       newBoard `shouldBe` (testGS_HandDeck0 & (playerState . element 0 . deck) .~ replicate 3 (NumberCard 2))
   describe "card8" $ do
     it "should work in basic case" $ do
-      let newBoard = playCard 0 card8 testGS_Board123
+      newBoard <- playCard 0 card8 testGS_Board123
       newBoard `shouldBe` testGS_All0
   describe "card9" $ do
     it "should work in basic case" $ do
       let gs = testGS_D1D2 (map NumberCard [1, 2]) (map NumberCard [1,2])
-      let newBoard = playCard 0 card9 gs
+      newBoard <- playCard 0 card9 gs
       let newDeck = map NumberCard [0, 1]
       newBoard ^.. (playerState . element 1 . deck . traverse) `shouldBe` newDeck
       newBoard `shouldBe` (gs & (playerState . element 1 . deck) .~ newDeck)
   describe "card10" $ do
     it "should work in basic case" $ do
       let gs = testGS_D1D2 (map NumberCard [1,2,3,4]) (map NumberCard [1,2,3,4])
-      let newBoard = playCard 0 (card10 (NumberCard 3) (NumberCard 4)) gs
+      newBoard <- playCard 0 (card10 (NumberCard 3) (NumberCard 4)) gs
       let newDeck = map NumberCard [3,4,1,2]
       newBoard ^.. (playerState . element 0 . deck . traverse) `shouldBe` newDeck
       newBoard `shouldBe` (gs & (playerState . element 0 . deck) .~ newDeck)
   describe "card12" $ do
     it "should work in basic case" $ do
-      let newBoard = playCard 0 card12 testGS_Board123
+      newBoard <- playCard 0 card12 testGS_Board123
       newBoard `shouldBe` (testGS_Board123 & (playerState . element 0 . board . element 2) .~ NumberCard 8)
+
